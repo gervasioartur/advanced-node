b02b90ec5b744d5062ca87145c5ab749130b5eff
gervasioartur <gervasioartur@outlook.com.br>
2022-07-01 00:14:10 -0300

    feat add facebook authenticaion features on damain layer

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,15 @@
+import { AccessToken } from '@/domain/models'
+import { AuthenticationError } from '@/domain/errors/authenticatio-error'
+
+namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
+
+  export type Result = AccessToken | AuthenticationError
+
+}
+
+export interface FacebookAuthentication {
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
