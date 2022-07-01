d29db72fd046c0b60e08155f34e62ff019449d68
gervasioartur <gervasioartur@outlook.com.br>
2022-07-01 10:23:20 -0300

    test: ensure LoadFacebookApi calls LoadAccountRepo with correct values

--- __tests__/data/services/facebook-authentication.spec.ts
+++ __tests__/data/services/facebook-authentication.spec.ts
@@ -2,25 +2,45 @@ import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthenticationService } from '@/data/services'
 import { mock, MockProxy } from 'jest-mock-extended'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { LoadUserAccountRepository } from '@/data/contracts/repos'
 
 describe('FacebookAuthenticationService', () => {
-  let loadFacebookUser: MockProxy<LoadFacebookUserApi>
   let sut: FacebookAuthenticationService
+  let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  let loadUserAccountRepo: MockProxy<LoadUserAccountRepository>
   const token = 'any_token'
+  const email = 'any_facebook_email'
 
   beforeEach(() => {
-    loadFacebookUser = mock()
-    sut = new FacebookAuthenticationService(loadFacebookUser)
+    loadFacebookUserApi = mock()
+    loadFacebookUserApi.loadUser.mockResolvedValue({
+      facebookId: 'any_facebook_id',
+      name: 'any_facebook_name',
+      email: 'any_facebook_email'
+    })
+    loadUserAccountRepo = mock()
+    sut = new FacebookAuthenticationService(
+      loadFacebookUserApi,
+      loadUserAccountRepo
+    )
   })
+
   it('should call LoadFacebookApi with correct values', async () => {
     await sut.perfom({ token })
-    expect(loadFacebookUser.loadUser).toHaveBeenCalledWith({ token })
-    expect(loadFacebookUser.loadUser).toHaveBeenCalledTimes(1)
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return authentication error when LoadFacebookApi returns undefined', async () => {
-    loadFacebookUser.loadUser.mockResolvedValueOnce(undefined)
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
     const authResult = await sut.perfom({ token })
     expect(authResult).toEqual(new AuthenticationError())
+  })
+
+  it('should call LoadUserAccountRepo with correct values when LoadFacebookApi return data', async () => {
+    await sut.perfom({ token })
+    await loadUserAccountRepo.load({ email })
+    expect(loadUserAccountRepo.load).toHaveBeenCalledWith({ email })
+    expect(loadUserAccountRepo.load).toHaveBeenCalledTimes(1)
   })
 })
