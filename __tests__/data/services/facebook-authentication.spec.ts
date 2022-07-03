7bfa0c7776fbb20e60fec0cf9051df5b971d57cc
gervasioartur <gervasioartur@outlook.com.br>
2022-07-02 22:10:45 -0300

    feat: ensure Service return an accessToken on success

--- __tests__/data/services/facebook-authentication.spec.ts
+++ __tests__/data/services/facebook-authentication.spec.ts
@@ -29,6 +29,7 @@ describe('FacebookAuthenticationService', () => {
     userAccountRepo.load.mockResolvedValue(undefined)
     userAccountRepo.saveWithFacebook.mockResolvedValue({ id: 'any_account_id' })
     crypto = mock()
+    crypto.generateToken.mockResolvedValue('any_generated_token')
     sut = new FacebookAuthenticationService(
       facebookApi,
       userAccountRepo,
@@ -37,19 +38,19 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('should call LoadFacebookApi with correct values', async () => {
-    await sut.perfom({ token })
+    await sut.perform({ token })
     expect(facebookApi.loadUser).toHaveBeenCalledWith({ token })
     expect(facebookApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return authentication error when LoadFacebookApi returns undefined', async () => {
     facebookApi.loadUser.mockResolvedValueOnce(undefined)
-    const authResult = await sut.perfom({ token })
+    const authResult = await sut.perform({ token })
     expect(authResult).toEqual(new AuthenticationError())
   })
 
   it('should call LoadUserAccountRepo with correct values when LoadFacebookApi returns data', async () => {
-    await sut.perfom({ token })
+    await sut.perform({ token })
     expect(userAccountRepo.load).toHaveBeenCalledWith({ email: 'any_facebook_email' })
     expect(userAccountRepo.load).toHaveBeenCalledTimes(1)
   })
@@ -57,17 +58,22 @@ describe('FacebookAuthenticationService', () => {
   it('should call SaveFacebookAccountRepository with correct FacebookAccount', async () => {
     const FacebookAccountStub = jest.fn().mockImplementation(() => ({ any: 'any' }))
     mocked(FacebookAccount).mockImplementation(FacebookAccountStub)
-    await sut.perfom({ token })
+    await sut.perform({ token })
     expect(userAccountRepo.saveWithFacebook).toHaveBeenCalledWith({ any: 'any' })
     expect(userAccountRepo.saveWithFacebook).toHaveBeenCalledTimes(1)
   })
 
   it('should call TokenGenerator  with correct params', async () => {
-    await sut.perfom({ token })
+    await sut.perform({ token })
     expect(crypto.generateToken).toHaveBeenCalledWith({
       key: 'any_account_id',
       expirationInMs: AccessToken.expirationInMs
     })
     expect(crypto.generateToken).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return an accessToken on success', async () => {
+    const authResult = await sut.perform({ token })
+    expect(authResult).toEqual(new AccessToken('any_generated_token'))
   })
 })
