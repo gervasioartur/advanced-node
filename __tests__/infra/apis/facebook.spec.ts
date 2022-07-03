cdb360b41ccf1800dcbd134a67cdd6d50b5d93b2
gervasioartur <gervasioartur@outlook.com.br>
2022-07-03 14:10:58 -0300

    test: ensure FacebookApi gets debug token

--- __tests__/infra/apis/facebook.spec.ts
+++ __tests__/infra/apis/facebook.spec.ts
@@ -15,6 +15,7 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
+    httClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
     sut = new FacebookApi(httClient, clientId, clientSecret)
   })
 
@@ -26,6 +27,17 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  it('should get debug token ', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
