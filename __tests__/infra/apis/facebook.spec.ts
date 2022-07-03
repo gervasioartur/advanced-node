7d461ed0ede2d37af90d490624f79cefb1ba928d
gervasioartur <gervasioartur@outlook.com.br>
2022-07-03 17:55:57 -0300

    feat: ensure FacebookApi get user info

--- __tests__/infra/apis/facebook.spec.ts
+++ __tests__/infra/apis/facebook.spec.ts
@@ -15,7 +15,10 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
-    httClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+    httClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+
     sut = new FacebookApi(httClient, clientId, clientSecret)
   })
 
@@ -38,6 +41,17 @@ describe('FacebookApi', () => {
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  })
+
+  it('should get userinfo', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   })
