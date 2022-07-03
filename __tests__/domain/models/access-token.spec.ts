fd773a1efa977b576e289bbaecbea521bc01f36d
gervasioartur <gervasioartur@outlook.com.br>
2022-07-02 21:48:50 -0300

    test: ensure AccessToken has a value

---
+++ __tests__/domain/models/access-token.spec.ts
@@ -0,0 +1,8 @@
+import { AccessToken } from '@/domain/models'
+
+describe('AccessToken', () => {
+  it('should  create with a value', () => {
+    const sut = new AccessToken('any_value')
+    expect(sut).toEqual({ value: 'any_value' })
+  })
+})
