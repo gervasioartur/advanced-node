36ef1e532a297cda0d7dc7d17d57f7694413973e
gervasioartur <gervasioartur@outlook.com.br>
2022-07-03 19:09:44 -0300

    refactor: insolate sut and varialbes creation

--- __tests__/infra/http/axios-client.spec.ts
+++ __tests__/infra/http/axios-client.spec.ts
@@ -10,22 +10,25 @@ export class AxiosHttpClient {
 }
 
 describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+
+  beforeAll(() => {
+    fakeAxios = axios as jest.Mocked<typeof axios>
+    url = 'any_url'
+    params = { any: 'any_params' }
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
+
   describe('get', () => {
     it('should call get with correct params', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosHttpClient()
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any: 'any_params'
-        }
-      })
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any: 'any_params'
-        }
-      })
-
+      await sut.get({ url, params })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
   })
