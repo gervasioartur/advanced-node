7b284577e563f0a0c9960ca9e62112f8aa89d7b8
gervasioartur <gervasioartur@outlook.com.br>
2022-07-03 19:17:57 -0300

    test: ensure AxiosHttpclient rethows if get throws

--- __tests__/infra/http/axios-client.spec.ts
+++ __tests__/infra/http/axios-client.spec.ts
@@ -38,5 +38,11 @@ describe('AxiosHttpClient', () => {
       const result = await sut.get({ url, params })
       expect(result).toEqual('any_data')
     })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValue(new Error('http_error'))
+      const promise = sut.get({ url, params })
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
