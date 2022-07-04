50f63759d683b9aa5f50def5a9a7be3cd6747d2b
gervasioartur <gervasioartur@outlook.com.br>
2022-07-04 08:42:52 -0300

    refactor: move SUT create to be used by another tests blocks

--- __tests__/infra/postgres/repos/user-acount.spec.ts
+++ __tests__/infra/postgres/repos/user-acount.spec.ts
@@ -5,26 +5,26 @@ import { getConnection, getRepository, Repository } from 'typeorm'
 import { makeFakeDb } from '../mocks'
 
 describe('PgUserAccountRepository', () => {
+  let sut: PgUserAccountRepository
+  let pgUserRepo: Repository<PgUser>
+  let backup: IBackup
+
+  beforeAll(async () => {
+    const db = await makeFakeDb()
+    backup = db.backup()
+    pgUserRepo = getRepository(PgUser)
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
+  beforeEach(() => {
+    backup.restore()
+    sut = new PgUserAccountRepository()
+  })
+
   describe('load', () => {
-    let sut: PgUserAccountRepository
-    let pgUserRepo: Repository<PgUser>
-    let backup: IBackup
-
-    beforeAll(async () => {
-      const db = await makeFakeDb()
-      backup = db.backup()
-      pgUserRepo = getRepository(PgUser)
-    })
-
-    afterAll(async () => {
-      await getConnection().close()
-    })
-
-    beforeEach(() => {
-      backup.restore()
-      sut = new PgUserAccountRepository()
-    })
-
     it('should return an account if email exists', async () => {
       await pgUserRepo.save({ email: 'existintg_email' })
       const account = await sut.load({ email: 'existintg_email' })
