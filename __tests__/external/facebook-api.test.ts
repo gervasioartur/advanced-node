aa2a467aba3cfc27fe2587ce8b234d59a966d112
gervasioartur <gervasioartur@outlook.com.br>
2022-07-04 20:25:19 -0300

    reafctor: move SUT creation to each to describe

--- __tests__/external/facebook-api.test.ts
+++ __tests__/external/facebook-api.test.ts
@@ -1,15 +1,21 @@
 import { FacebookApi } from '@/infra/apis'
 import { AxiosHttpClient } from '@/infra/http'
 import { env } from '@/main/config/env'
+
 describe('facebook Api Integration Tests', () => {
-  it('should return a facebook user if token is valid', async () => {
-    const axioClient = new AxiosHttpClient()
-    const sut = new FacebookApi(
+  let sut: FacebookApi
+  let axioClient: AxiosHttpClient
+  beforeEach(() => {
+    axioClient = new AxiosHttpClient()
+    sut = new FacebookApi(
       axioClient,
       env.facebookApi.clientId,
       env.facebookApi.clientSecret
     )
-    const fbUser = await sut.loadUser({ token: 'EAAFX9uOG26gBAMVstFXRW8kyTZCIhUW1tWZCATV3o7ZCoRIbjR077RiNroQ71WLnf7GrhbihTnjlIzjWxMtin2RDs5NFmMfw3CSvWRTXfKzlfLYKGXaacyNz2ZCwSpYUc6vSlYiBKrZCH91kPnmhLtJwoozc5anvmmnJxuI9fuTSYidbP4jaTHY0QNLqsZC40KIw742lQZCQQZDZD' })
+  })
+
+  it('should return a facebook user if token is valid', async () => {
+    const fbUser = await sut.loadUser({ token: env.facebookApi.accessTokenInTestMode })
     expect(fbUser).toEqual({
       facebookId: '100139682770806',
       email: 'gerry_sgxlsjq_teste@tfbnw.net',
@@ -18,12 +24,6 @@ describe('facebook Api Integration Tests', () => {
   })
 
   it('should return undefined if token is invalid', async () => {
-    const axioClient = new AxiosHttpClient()
-    const sut = new FacebookApi(
-      axioClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    )
     const fbUser = await sut.loadUser({ token: 'invali_token' })
     expect(fbUser).toBeUndefined()
   })
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,8 @@
 export const env = {
   facebookApi: {
     clientId: process.env.FACEBOOK_CLIENT_ID ?? '378192867548072',
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? '3f21b8e402e95448cf7b84b875d75b7e'
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? '3f21b8e402e95448cf7b84b875d75b7e',
+    // this token expires in 3 moths
+    accessTokenInTestMode: 'EAAFX9uOG26gBAMVstFXRW8kyTZCIhUW1tWZCATV3o7ZCoRIbjR077RiNroQ71WLnf7GrhbihTnjlIzjWxMtin2RDs5NFmMfw3CSvWRTXfKzlfLYKGXaacyNz2ZCwSpYUc6vSlYiBKrZCH91kPnmhLtJwoozc5anvmmnJxuI9fuTSYidbP4jaTHY0QNLqsZC40KIw742lQZCQQZDZD'
   }
 }
