f84603f3453e475da281f51221d60511e70d1e35
gervasioartur <gervasioartur@outlook.com.br>
2022-07-06 14:28:37 -0300

    test: ensure generate JwtTokenHanlder calls validateToken with correct values

--- __tests__/infra/crypto/token-handler.spec.ts
+++ __tests__/infra/crypto/token-handler.spec.ts
@@ -9,6 +9,7 @@ describe('JwtTokenHandler', () => {
   let fakeJwt: jest.Mocked<typeof jwt>
 
   beforeEach(() => {
+    secret = 'any_secret'
     sut = new JwtTokenHandler('any_secret')
   })
 
@@ -20,17 +21,14 @@ describe('JwtTokenHandler', () => {
       key = 'any_key'
       token = 'any_token'
       expirationInMs = 1000
-      secret = 'any_secret'
       fakeJwt = jwt as jest.Mocked<typeof jwt>
       fakeJwt.sign.mockImplementation(() => token)
     })
 
     it('should call sing with corecct params', async () => {
       await sut.generateToken({ key, expirationInMs })
-      expect(fakeJwt.sign).toHaveBeenCalledWith(
-        { key },
-        secret,
-        { expiresIn: 1 })
+      expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: 1 })
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
     })
 
     it('should return a token', async () => {
@@ -42,6 +40,20 @@ describe('JwtTokenHandler', () => {
       fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error') })
       const promise = sut.generateToken({ key, expirationInMs })
       await expect(promise).rejects.toThrow(new Error('token_error'))
+    })
+  })
+
+  describe('validateToken', () => {
+    let token: string
+    beforeAll(() => {
+      token = 'any_token'
+      fakeJwt.sign.mockImplementation(() => token)
+    })
+
+    it('should call sing with corecct params', async () => {
+      await sut.validateToken({ token })
+      expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret)
+      expect(fakeJwt.verify).toHaveBeenCalledTimes(1)
     })
   })
 })
